4becfa46763da2ef9fb18f40654991bad0f7ee84
Jay <jaykang445@gmail.com>
2024-01-08 14:19:28 -0800

    remove last season code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,10 +17,6 @@ import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.Filesystem;
 import frc.robot.Constants.Drive.Dims;
-import frc.robot.Constants.Elevator;
-import frc.robot.commands.ScoreCommand.ScoreStep;
-import frc.robot.subsystems.ElevatorSubsystem.ElevatorState;
-import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.NetworkWatchdogSubsystem.IPv4;
 import frc.robot.subsystems.RGBSubsystem.RGBColor;
 import frc.robot.subsystems.VisionSubsystem.TagCountDeviation;
@@ -54,7 +50,7 @@ public final class Constants {
             && HALUtil.getHALRuntimeType() != HALUtil.RUNTIME_SIMULATION;
 
     /** turn this off before comp. */
-    public static final boolean SHOW_SHUFFLEBOARD_DEBUG_DATA = false;
+    public static final boolean SHOW_SHUFFLEBOARD_DEBUG_DATA = true;
 
     /** turn this off! only use on practice eboard testing. */
     public static final boolean DISABLE_SWERVE_MODULE_INIT = false;
@@ -69,9 +65,6 @@ public final class Constants {
   }
 
   public static final class Drive {
-
-    public static final int ZAPPY_THING_PORT = 0; // FIXME Placeholder
-
     // max voltage delivered to drivebase
     // supposedly useful to limit speed for testing
     public static final double MAX_VOLTAGE = 12.0;
@@ -102,15 +95,6 @@ public final class Constants {
       public static final double WHEELBASE_METERS = TRACKWIDTH_METERS; // robot is square
 
       public static final double BUMPER_WIDTH_METERS = .851;
-    }
-
-    public static final class AutoBalance {
-      public static final double DOCK_SPEED_METERS_PER_SECOND = .798;
-      public static final double DOCK_MIN_ANGLE_DEGREES = 13.717;
-      public static final double DOCK_HORIZON_ANGLE_DEGREES = 17.532 - 2;
-
-      public static final double ENGAGE_SPEED_METERS_PER_SECOND = .403;
-      public static final double ENGAGE_MIN_ANGLE_DEGREES = 9.365;
     }
 
     /*
@@ -190,111 +174,6 @@ public final class Constants {
       }
     }
   }
-  // FIXME fix all these numbers, don't know any of them
-  public static final class Elevator {
-    public static final class Ports {
-
-      public static final int ELEVATOR_LEFT_MOTOR_PORT = CAN.at(14, "elevator left motor");
-      public static final int ELEVATOR_RIGHT_MOTOR_PORT = CAN.at(15, "elevator right motor");
-      public static final int WRIST_MOTOR_PORT = CAN.at(16, "wrist motor port");
-    }
-
-    public static final class Setpoints {
-      public static final ElevatorState STOWED =
-          new ElevatorState(MIN_EXTENSION_INCHES, MIN_ANGLE_DEGREES);
-      public static final ElevatorState SHELF_INTAKE_CONE = new ElevatorState(47.3, 68.5);
-      public static final ElevatorState SHELF_INTAKE_CUBE = new ElevatorState(43.2, 68.5);
-      public static final ElevatorState GROUND_INTAKE_CONE =
-          new ElevatorState(MIN_EXTENSION_INCHES, 72.5);
-      public static final ElevatorState GROUND_INTAKE_CUBE =
-          new ElevatorState(MIN_EXTENSION_INCHES, 83);
-      public static final ElevatorState SCORE_HIGH_CONE = new ElevatorState(43, 52.5);
-      public static final ElevatorState SCORE_HIGH_CUBE = new ElevatorState(53.8, 74.5);
-      public static final ElevatorState SCORE_MID_CONE = new ElevatorState(22.5, 35.5);
-      public static final ElevatorState SCORE_MID_CUBE = new ElevatorState(30, 74.5);
-      public static final ElevatorState SCORE_LOW_CONE =
-          new ElevatorState(MIN_EXTENSION_INCHES, 55);
-      public static final ElevatorState SCORE_LOW_CUBE =
-          new ElevatorState(MIN_EXTENSION_INCHES, 48);
-    }
-
-    public static final double MAX_EXTENSION_INCHES = 54;
-    public static final double MIN_EXTENSION_INCHES = 1;
-
-    public static final double MIN_ANGLE_DEGREES = 5;
-    public static final double MAX_ANGLE_DEGREES = 109.25;
-
-    public static final int FALCON_CPR = 2048;
-    public static final double ELEVATOR_GEAR_RATIO = 0.1008;
-    public static final double ELEVATOR_SPROCKET_DIAMETER_INCHES = 1.432;
-    public static final double CARRIAGE_RATIO = 2;
-
-    public static final int WRIST_TICKS = 2048;
-    public static final double WRIST_DEGREES = 360;
-    public static final double WRIST_GEAR_RATIO = 1 / 38.6719;
-
-    public static final double ANGLE_EPSILON = 0.5;
-    public static final double EXTENSION_EPSILON = 5;
-    public static final double ANGULAR_OFFSET = 0;
-
-    public static final double ZERO_MOTOR_POWER = -0.12;
-    public static final double ZERO_STATOR_LIMIT = 7;
-    public static final double STATOR_LIMIT = 25;
-
-    public static final double GRAVITY_OFFSET_PERCENT = .2;
-  }
-
-  public static final class Intake {
-
-    public static final double INTAKE_CONE_PERCENT = 0.7;
-    public static final double INTAKE_CUBE_PERCENT = -0.7;
-
-    public static final double OUTTAKE_CONE_PERCENT = -0.7;
-    public static final double OUTTAKE_CUBE_PERCENT = 0.7;
-
-    public static final double HOLD_CONE_PERCENT = 0.1;
-    public static final double HOLD_CUBE_PERCENT = -0.2;
-
-    public static final double CONE_STATOR_LIMIT = 75;
-    public static final double CUBE_STATOR_LIMIT = 69;
-    public static final double GROUND_CUBE_STATOR_LIMIT = 60;
-
-    public static final class Ports {
-      public static final int INTAKE_MOTOR_PORT = CAN.at(18, "intake motor");
-      public static final int CONE_TOF_PORT = 0;
-      public static final int CUBE_TOF_PORT = 0;
-    }
-  }
-
-  public static final Map<ScoreTypeIdentifier, List<ScoreStep>> SCORE_STEP_MAP =
-      Map.of(
-          NodeType.CONE.atHeight(Height.HIGH),
-          List.of(
-              new ScoreStep(Elevator.Setpoints.SCORE_HIGH_CONE).canWaitHere(),
-              new ScoreStep(new ElevatorState(22.5, 0), IntakeSubsystem.Modes.OUTTAKE, false)),
-          NodeType.CONE.atHeight(Height.MID),
-          List.of(
-              new ScoreStep(Elevator.Setpoints.SCORE_MID_CONE).canWaitHere(),
-              new ScoreStep(
-                  Elevator.Setpoints.SCORE_MID_CONE, IntakeSubsystem.Modes.OUTTAKE, false)),
-          NodeType.CONE.atHeight(Height.LOW),
-          List.of(
-              new ScoreStep(Elevator.Setpoints.SCORE_LOW_CONE).canWaitHere(),
-              new ScoreStep(IntakeSubsystem.Modes.OUTTAKE, false)),
-          NodeType.CUBE.atHeight(Height.HIGH),
-          List.of(
-              new ScoreStep(Elevator.Setpoints.SCORE_HIGH_CUBE).canWaitHere(),
-              new ScoreStep(
-                  Elevator.Setpoints.SCORE_HIGH_CUBE, IntakeSubsystem.Modes.OUTTAKE, true)),
-          NodeType.CUBE.atHeight(Height.MID),
-          List.of(
-              new ScoreStep(Elevator.Setpoints.SCORE_MID_CUBE).canWaitHere(),
-              new ScoreStep(
-                  Elevator.Setpoints.SCORE_MID_CUBE, IntakeSubsystem.Modes.OUTTAKE, true)),
-          NodeType.CUBE.atHeight(Height.LOW),
-          List.of(
-              new ScoreStep(Elevator.Setpoints.SCORE_LOW_CUBE).canWaitHere(),
-              new ScoreStep(IntakeSubsystem.Modes.OUTTAKE, true)));
 
   public static final class Vision {
     public static record VisionSource(String name, Transform3d robotToCamera) {}
