307fc48e264d11f48ab812298493987e4d140024
Jay <jaykang445@gmail.com>
2024-01-23 16:58:13 -0800

    actually thats unnecessary

--- src/main/java/frc/robot/subsystems/DrivebaseSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivebaseSubsystem.java
@@ -212,7 +212,7 @@ public class DrivebaseSubsystem extends SubsystemBase {
         this::getPose,
         this::resetOdometryToPose,
         this::getRobotRelativeSpeeds,
-        (speeds) -> this.drive(speeds),
+        this::drive,
         Constants.Config.PATH_FOLLOWER_CONFIG,
         () -> {
           // Boolean supplier that controls when the path will be mirrored for the red alliance
