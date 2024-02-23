f772547f5b2407a76400f6d7af57de9f0468577e
Ethan Li <ethanchili224@gmail.com>
2024-02-22 17:37:59 -0800

    umm brandon told me to do this
    what does it do brandon soz please help

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -20,8 +20,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Config;
 import frc.robot.Constants.Shooter;
 import frc.robot.Constants.Shooter.Setpoints;
-import java.util.Optional;
 import frc.util.Util;
+import java.util.Optional;
 
 public class ShooterSubsystem extends SubsystemBase {
   private final TalonFX wristMotor;
