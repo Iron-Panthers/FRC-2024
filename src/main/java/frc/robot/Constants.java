702ca63aece7c0d7fcdd0c246a21ffaa4fe9e63e
Jay <jaykang445@gmail.com>
2024-03-02 15:45:52 -0800

    correct pivot encoder offset

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -318,7 +318,7 @@ public final class Constants {
 
     public static final int EPSILON = 2;
 
-    public static final double PIVOT_CANCODER_OFFSET = -0.281250 + (0.046387 - 0.012695);
+    public static final double PIVOT_CANCODER_OFFSET = -0.610840 + (0.395264 - 0.363525);
     public static final double PIVOT_GEAR_RATIO =
         (60 / 8) * (60 / 16) * (72 / 15); // FIXME placeholder values
 
