9081ead2f158d2376001ec0e7d2640ed0768f627
audrey <audreypjohnson@gmail.com>
2024-03-17 13:51:57 -0700

    adjust constants bc the last commit is messed up??

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -342,8 +342,8 @@ public final class Constants {
         (60 / 8) * (60 / 16) * (72 / 15); // FIXME placeholder values
     public static final double CENTER_OF_ROBOT_TO_BUMPER = 0.41275;
 
-    public static final Pose2d RED_SPEAKER_POSE = new Pose2d(16.45, 6, null);
-    public static final Pose2d BLUE_SPEAKER_POSE = new Pose2d(0.2, 6, null);
+    public static final Pose2d RED_SPEAKER_POSE = new Pose2d(16.45, 5.5, null);
+    public static final Pose2d BLUE_SPEAKER_POSE = new Pose2d(0, 5.5, null);
 
     public static final double GRAVITY = 9.80665; // meters per second
 
